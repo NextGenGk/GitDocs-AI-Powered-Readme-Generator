09257e832dd1d9ab29703a60ec0428aa33806643
AI PC <atkola12345@gmail.com>
2025-06-04 23:44:04 +0530

    change components folder from app -> outer --changes

---
+++ components/Banner.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function () {
+    return (
+        <div>
+            <Image
+                src="/banner.jpg"
+                alt="banner"
+                width={0}
+                height={0}
+                sizes="100vw"
+                z-index={-1}
+                style={{ width: '100%', height: '100vh', opacity: 0.80, objectFit: 'cover'}}
+            />
+        </div>
+    )
+}
