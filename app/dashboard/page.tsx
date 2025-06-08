c54a0302577ff80e818a07039ae431fb102c4f73
AI PC <atkola12345@gmail.com>
2025-06-08 14:21:48 +0530

    add pages dashboard, faq and pricing

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import BasicFeature from "@/components/BasicFeature";
+
+export default function Dashboard() {
+    return (
+        <main className="min-h-screen flex items-center justify-center p-4">
+            <BasicFeature />
+        </main>
+    );
+}
---
+++ app/faq/page.tsx
@@ -0,0 +1,9 @@
+import Faq from "@/components/Faq";
+
+export default function FAQ() {
+    return (
+        <main className="min-h-screen flex items-center justify-center p-4">
+            <Faq />
+        </main>
+    )
+}
