265cb14ce6cbd1b0dadbe11216e6ef2b009f2cf9
AI PC <atkola12345@gmail.com>
2025-06-12 22:47:30 +0530

    update Dashboard Page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,15 @@
-"use server"; // Required for hooks like useUser
-
-import ProFeature from "@/components/ProFeature";
 import BasicFeature from "@/components/BasicFeature"; // Ensure BasicFeature is imported
-import { auth } from "@clerk/nextjs/server";
-import Link from "next/link";
 
 export default async function Dashboard() {
-    const { has } = await auth();
-
-    const hasProFeature = has({plan: "pro"});
 
     return (
         <main className="min-h-screen flex items-center justify-center p-4">
-            {hasProFeature ? (
-                    <div>
-                        <BasicFeature />
-                        <Link href="/pricing" className="mt-4 text-blue-500 hover:underline">
-                            Upgrade to Pro
-                        </Link>
-                    </div>
-            ) : (
-                <div>
-                    <ProFeature />
-                </div>
-            )}
+            <div>
+                <BasicFeature />
+                {/*<Link href="/pricing" className="mt-4 text-blue-500 hover:underline">*/}
+                {/*    Upgrade to Pro*/}
+                {/*</Link>*/}
+            </div>
         </main>
     );
 }
