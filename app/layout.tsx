0314c471c1f5a73e497aa0d83451ba121e5988d7
AI PC <atkola12345@gmail.com>
2025-06-19 19:32:20 +0530

    remove redirect sign-in from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,6 @@ export default function RootLayout({
           </SignedIn>
 
           <SignedOut>
-            <RedirectToSignIn />
             <Navbar />
             <main>
               {children}
